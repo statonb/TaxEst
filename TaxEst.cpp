9e47ece7c0f4074444e3d02356c43fdbeed9462a
Brian Staton <brian@carrarasoftware.com>
2024-02-04 18:02:00 -0500

    Print headroom in current tax bracket

--- TaxEst.cpp
+++ TaxEst.cpp
@@ -6,8 +6,8 @@
 #include <string.h>
 #include <getopt.h>
 
-const char *SW_VERSION =    "1.10";
-const char *SW_DATE =       "2024-01-24";
+const char *SW_VERSION =    "1.11";
+const char *SW_DATE =       "2024-02-04";
 
 #define MULTI_ELEMENT_TEST  (0)
 
@@ -16,6 +16,8 @@ const char *SW_DATE =       "2024-01-24";
 #define NUM_BRACKETS        (7)
 #define FIRST_YEAR          (2023)
 #define LAST_YEAR           (2024)
+
+#define MAX_UPPER_LIMIT     (999999)
 
 typedef enum
 {
@@ -101,7 +103,7 @@ bracketElement_t    taxBrackets[LAST_YEAR-FIRST_YEAR+1][NUM_FILING_STATUS][NUM_B
             {.24,   95376,    182100},
             {.32,   182101,   231250},
             {.35,   231251,   578125},
-            {.37,   578126,   999999}
+            {.37,   578126,   MAX_UPPER_LIMIT}
         },
         {
             //  2023 MFJ
@@ -111,7 +113,7 @@ bracketElement_t    taxBrackets[LAST_YEAR-FIRST_YEAR+1][NUM_FILING_STATUS][NUM_B
             {.24,   190751,    364200},
             {.32,   364201,    462500},
             {.35,   462501,    693750},
-            {.37,   693751,    999999}
+            {.37,   693751,    MAX_UPPER_LIMIT}
         }
     },
     {
@@ -124,7 +126,7 @@ bracketElement_t    taxBrackets[LAST_YEAR-FIRST_YEAR+1][NUM_FILING_STATUS][NUM_B
             {.24,   100526,   191950},
             {.32,   191951,   243725},
             {.35,   243726,   609350},
-            {.37,   690351,   999999}
+            {.37,   690351,   MAX_UPPER_LIMIT}
         },
         {
             //  2024 MFJ
@@ -134,7 +136,7 @@ bracketElement_t    taxBrackets[LAST_YEAR-FIRST_YEAR+1][NUM_FILING_STATUS][NUM_B
             {.24,   201051,    383900},
             {.32,   383901,    487450},
             {.35,   487451,    731200},
-            {.37,   731201,    999999}
+            {.37,   731201,    MAX_UPPER_LIMIT}
         }
     }
 };
@@ -148,7 +150,7 @@ bracketElement_t    taxBracket_SINGLE_2023[NUM_BRACKETS] =
     {.24,   95376,    182100},
     {.32,   182101,   231250},
     {.35,   231251,   578125},
-    {.37,   578126,   999999}
+    {.37,   578126,   MAX_UPPER_LIMIT}
 };
 
 bracketElement_t    taxBracket_MFJ_2023[NUM_BRACKETS] =
@@ -159,7 +161,7 @@ bracketElement_t    taxBracket_MFJ_2023[NUM_BRACKETS] =
     {.24,   190751,    364200},
     {.32,   364201,    462500},
     {.35,   462501,    693750},
-    {.37,   693751,    999999}
+    {.37,   693751,    MAX_UPPER_LIMIT}
 };
 
 bracketElement_t    taxBracket_SINGLE_2024[NUM_BRACKETS] =
@@ -170,7 +172,7 @@ bracketElement_t    taxBracket_SINGLE_2024[NUM_BRACKETS] =
     {.24,   100526,   191950},
     {.32,   191951,   243725},
     {.35,   243726,   609350},
-    {.37,   690351,   999999}
+    {.37,   690351,   MAX_UPPER_LIMIT}
 };
 
 bracketElement_t    taxBracket_MFJ_2024[NUM_BRACKETS] =
@@ -181,7 +183,7 @@ bracketElement_t    taxBracket_MFJ_2024[NUM_BRACKETS] =
     {.24,   201051,    383900},
     {.32,   383901,    487450},
     {.35,   487451,    731200},
-    {.37,   731201,    999999}
+    {.37,   731201,    MAX_UPPER_LIMIT}
 };
 #endif
 
@@ -252,22 +254,10 @@ int main(int argc, char *argv[])
             break;
         case 'y':
             taxYear = strtoul(optarg, NULL, 10);
-            if  (   (taxYear < FIRST_YEAR)
-                 || (taxYear > LAST_YEAR)
-                )
-            {
-                usageError = true;
-            }
             break;
         case 'i':
         case 't':
             taxableIncome = strtoul(optarg, NULL, 10);
-            if  (   (taxableIncome == 0)
-                 || (taxableIncome > 999999)
-                )
-            {
-                usageError = true;
-            }
             break;
         case 'g':
             grossIncome = strtoul(optarg, NULL, 10);
@@ -292,6 +282,22 @@ int main(int argc, char *argv[])
     // ----------- END OF TEST CODE
     #endif
 
+    if  (   (taxableIncome == 0)
+         || (taxableIncome > MAX_UPPER_LIMIT)
+        )
+    {
+        usageError = true;
+    }
+    if (taxYear < FIRST_YEAR)
+    {
+        taxYear += 2000;
+    }
+    if  (   (taxYear < FIRST_YEAR)
+         || (taxYear > LAST_YEAR)
+        )
+    {
+        usageError = true;
+    }
     if (usageError)
     {
         usage(basename(argv[0]));
@@ -337,6 +343,19 @@ int main(int argc, char *argv[])
             printf("Effective Rate: %0.2lf%%\n", tax / (double)grossIncome * 100.0);
         }
         printf("\n *** Tax ***  : $%.2lf\n", tax);
+        if (MAX_UPPER_LIMIT == pTaxBracket->upperLimit)
+        {
+            printf("\nMax Tax Bracket\n");
+        }
+        else
+        {
+            pTaxBracket++;
+            printf("\n%.0lf%% tax bracket starts at $%d.  Headroom is $%d\n"
+                , pTaxBracket->rate * 100.0
+                , pTaxBracket->lowerLimit
+                , pTaxBracket->lowerLimit - taxableIncome
+                );
+        }
     }
     else
     {
